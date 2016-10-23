d701f6018a129e08cbdbdbf15347253d608b4aa3
HaikalM <maulana.haikal.98@gmail.com>
2016-10-23 18:16:33 +0800

    Add validation cant be blank to user field in Microposts

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -1,4 +1,5 @@
 class Micropost < ApplicationRecord
 	belongs_to :user
-	validates :content, length: { maximum: 140},presence: true
+	validates :content, length: { maximum: 140}
+	validates :content , :user_id , presence: true
 end
